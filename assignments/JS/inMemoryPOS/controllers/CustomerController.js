30cd8481d5a9922420d8082a5fefb4b08e568f7e
SupunWellappili <supunchanada1814@gmail.com>
2023-10-17 17:46:51 +0530

    Create Object

--- assignments/JS/inMemoryPOS/controllers/CustomerController.js
+++ assignments/JS/inMemoryPOS/controllers/CustomerController.js
@@ -1,7 +1,20 @@
-let customerID = $('cusId').val();
-let customerName = $('custName').val();
-let customerAddress = $('custAddress').val();
-let customerSalary = $('custSalary').val();
+
+var customers= [];// Create a Array in global Scope
+var customerObject;
+
+$('btnSave').click(function () {
+    let customerID = $('cusId').val();
+    let customerName = $('custName').val();
+    let customerAddress = $('custAddress').val();
+    let customerSalary = $('custSalary').val();
 
 
+    customerObject = {
+        id: customerID,
+        name: customerName,
+        address: customerAddress,
+        salary: customerSalary
+    }
 
+    customers.push(customerObject);
+});
