3e7ea356981d1384fc71317f9f9d16a8df47cbe0
SupunWellappili <supunchanada1814@gmail.com>
2023-10-17 23:12:34 +0530

    Add to customer table

--- assignments/JS/inMemoryPOS/controllers/CustomerController.js
+++ assignments/JS/inMemoryPOS/controllers/CustomerController.js
@@ -2,11 +2,11 @@
 var customers= [];// Create a Array in global Scope
 var customerObject;
 
-$('btnSave').click(function () {
-    let customerID = $('cusId').val();
-    let customerName = $('custName').val();
-    let customerAddress = $('custAddress').val();
-    let customerSalary = $('custSalary').val();
+$('#btnSave').click(function () {
+    let customerID = $('#custId').val();
+    let customerName = $('#custName').val();
+    let customerAddress = $('#custAddress').val();
+    let customerSalary = $('#custSalary').val();
 
 
     customerObject = {
@@ -17,4 +17,26 @@ $('btnSave').click(function () {
     }
 
     customers.push(customerObject);
+
+
+
+//////////////////////////////////Customer Table///////////////////////////////////////////
+
+    function getAllCustomer() {
+
+        for (var i of customers) {
+            var row = `<tr><td>${i.id}</td><td>${i.name} </td><td>${i.address}</td><td> ${i.salary}</td></tr>`;
+
+            $('#tblCustomer').append(row);
+        }
+    }
+
+
+    getAllCustomer();
+
 });
+
+
+
+
+
