42233949bb345d3b39d34336cad24a53ab5e0645
SupunWellappili <supunchanada1814@gmail.com>
2023-10-23 13:09:59 +0530

    Create a  loading gif

--- assignments/JS/inMemoryPOS/controllers/DashboardController.js
+++ assignments/JS/inMemoryPOS/controllers/DashboardController.js
@@ -6,7 +6,7 @@
 $(window).on('load', function () {
     // console.log("Window in load");
     // $('#loader').css('display', 'none');
-    $('#loader').fadeOut(1000);
+    $('#loader').fadeOut(10000);
 });
 
 
