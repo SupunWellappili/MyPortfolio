16ad4993e9e97d489eb8a6aeb348cae050f5e3c3
SupunWellappili <supunchanada1814@gmail.com>
2023-10-24 13:21:56 +0530

    create add item & action Event

--- assignments/JS/inMemoryPOS/controllers/ItemController.js
+++ assignments/JS/inMemoryPOS/controllers/ItemController.js
@@ -14,7 +14,33 @@ $("#btnSaveItem").click(function () {
         quantity: itQuantity
     };
 
+
     items.push(itemObject);
+    bindRowClickEvents();
+    getAllCustomer();
 });
 
+////////////////////////////////Save & Get All Buttons////////////////////////////////////////////////
 
+function bindRowClickEvents() {
+    $('#tblItem>tr').click(function () {
+        let code = $(this).children().eq(0).text();
+        let name = $(this).children().eq(1).text();
+        let price = $(this).children().eq(2).text();
+        let quantity = $(this).children().eq(3).text();
+
+        $('#itCode').val(code);
+        $('#itName').val(name);
+        $('#itUnitPrice').val(price);
+        $('#itQuantity').val(quantity);
+    })
+};
+
+
+function getAllCustomer() {
+    $('#tblItem').empty();
+    for (var i of items) {
+        var row = `<tr><td>${i.code}</td><td>${i.name} </td><td>${i.price}</td><td> ${i.quantity}</td></tr>`;
+        $('#tblItem').append(row);
+    }
+}
