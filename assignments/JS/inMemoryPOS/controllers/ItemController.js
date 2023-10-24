965fff5c925eb0a74025b52fbafd35d9463d925c
SupunWellappili <supunchanada1814@gmail.com>
2023-10-24 12:20:21 +0530

    Create item objects

---
+++ assignments/JS/inMemoryPOS/controllers/ItemController.js
@@ -0,0 +1,19 @@
+var itemObject;
+$("#btnSaveItem").click(function () {
+    let itCode = $('#itemCode').val();
+    let itName = $('#itemName').val();
+    let itUnitePrice = $('#itemUnitPrice').val();
+    let itQuantity = $('#itemQuantity').val();
+
+
+    itemObject = {
+        code: itCode,
+        name: itName,
+        price: itUnitePrice,
+        quantity: itQuantity
+    };
+
+
+});
+
+
