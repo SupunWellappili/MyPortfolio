536501b9a3ac780d45b53e6ac396ffa14e3bdec9
SupunWellappili <supunchanada1814@gmail.com>
2023-10-25 20:34:52 +0530

    Create a Customer Validation(RegExp)

---
+++ assignments/JS/inMemoryPOS/controllers/validation/CustomerController.js
@@ -0,0 +1,9 @@
+
+$('#custId').focus();
+
+
+const cusIDRegEx = /^(C001-)[0-9]{1,3}$/;
+const cusNameRegEx = /^[A-z]{5,20}$/;
+const custAddressRegEx =/^[0-9/A-z. ,]{7,}$/;
+const cusSalaryRegEx =/^[0-9]{1,}[.]?[0-9]{1,2}$/;
+
