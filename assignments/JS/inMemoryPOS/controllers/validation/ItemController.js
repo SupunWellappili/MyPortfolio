38fe7633c4c2cc5b8a38b9def04a9977adf985aa
SupunWellappili <supunchanada1814@gmail.com>
2023-10-27 01:07:45 +0530

    Crete itemValidation arrys

--- assignments/JS/inMemoryPOS/controllers/validation/ItemController.js
+++ assignments/JS/inMemoryPOS/controllers/validation/ItemController.js
@@ -8,6 +8,11 @@ const itQuantity = /^[0-9]{1,}$/;
 
 
 
+let itemValidation = [];
+itemValidation.push({reg: itCodeRegEx, field: $('#itemCode'), error: 'Item Code pattern is  Wrong : (It-0001)'});
+itemValidation.push({reg: itNameRegEx, field: $('#itemName'), error: 'Item Name pattern is Wrong : (A-z)'});
+itemValidation.push({reg: itUnitPrice, field: $('#itemUnitPrice'), error: 'Item Unit Price pattern is Wrong : (0-9).(0-9){1,2}'});
+itemValidation.push({reg: itQuantity, field: $('#itemQuantity'), error: 'Item Quantity pattern is Wrong : 00'});
 
 
 
