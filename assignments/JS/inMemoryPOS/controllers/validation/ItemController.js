f1c896c410027f16a28a64ffcbb204c817547343
SupunWellappili <supunchanada1814@gmail.com>
2023-10-27 00:35:52 +0530

    Create item validation & asign variable

---
+++ assignments/JS/inMemoryPOS/controllers/validation/ItemController.js
@@ -0,0 +1,13 @@
+$('#itemCode').focus();
+
+
+const itCodeRegEx = /^(It-0)[0-9]{1,3}$/;
+const itNameRegEx = /^[A-z]{5,20}$/;
+const itUnitPrice = /^[0-9]{1,}[.]?[0-9]{1,2}$/;
+const itQuantity = /^[0-9]{1,}$/;
+
+
+
+
+
+
