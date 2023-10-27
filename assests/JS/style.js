73bdbd9cdaf72831a3a7208ab12c8b74bb16a638
SupunWellappili <supunchanada1814@gmail.com>
2023-10-27 12:55:39 +0530

    Loading page created

---
+++ assests/JS/style.js
@@ -0,0 +1,11 @@
+
+
+/////////////////////////////////Loading Time////////////////////////////////////////////////
+
+
+$(window).on('load', function () {
+// console.log("Window in load");
+// $('#portloader').css('display', 'none');
+    $('#portLoader').fadeOut(5000);
+});
+
